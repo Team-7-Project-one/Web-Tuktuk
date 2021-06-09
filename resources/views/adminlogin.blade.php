9ed6a2f348d3064b77ff3058eaa7e5ee4ac934c4
Wordyka Nainggolan <wordykanainggolan1212@gmail.com>
2021-06-09 23:24:25 +0700

    Admin Login Responsive (2)

--- resources/views/adminlogin.blade.php
+++ resources/views/adminlogin.blade.php
@@ -3,8 +3,8 @@
 <head>
     <link rel="icon" type="image/png" href="{{asset('img/logo_web.png')}}" />
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
+    <meta name="viewport" content="width=device-width">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,600,700&display=swap" rel="stylesheet" />
 
     <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css" />
@@ -26,8 +26,11 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous" />
     <style>
         body {
-            background-image: url('img/carousel2.jpg'); 
+            background-image: url('img/carousel2.jpg');
+            background-repeat: no-repeat;
             background-size: cover;
+            background-position: center center;
+            background-attachment: fixed;
         }
         .loginadmin{
             margin: 0 auto;
@@ -81,10 +84,6 @@
             <br><br><br>
         </form>
     </div>
-
-
-
-
 
     {{-- javascript link --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
